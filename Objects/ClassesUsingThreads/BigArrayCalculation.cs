ea098d52fbcdbd17bbcd8064055ed20f941ba917
Георгий Бербат <invariantcalibration@gmail.com>
2021-04-04 16:24:59 +0300

    Часть первой задачи поп потокам

--- Objects/ClassesUsingThreads/BigArrayCalculation.cs
+++ Objects/ClassesUsingThreads/BigArrayCalculation.cs
@@ -1,12 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace TwoLayerSolution.ClassesUsingThreads
 {
+    public enum SmallBig
+    {
+        Small, Big
+    }
+    public class ParametersForThread
+    {
+        public int start;
+        public int end;
+        public SmallBig whichArray;
+
+        public ParametersForThread(int start, int end, SmallBig whichArray)
+        {
+            this.start = start;
+            this.end = end;
+            this.whichArray = whichArray;
+        }
+    }
+
     public class BigArrayCalculation
     {
         private int[] _smallArray;
         private int[] _bigArray;
-
+        public List<double> AveragesForSmall;
+        public List<double> AveragesForBig;
         public void Generate()
         {
             var rand = new Random();
@@ -23,38 +45,68 @@ namespace TwoLayerSolution.ClassesUsingThreads
             }
         }
 
-        public double GetAverage(int[] array)
+        public void CalculateAverage(object obj)
         {
-            double avg = 0;
             double avgTemp = 0;
             int cnt = 0;
+
+            ParametersForThread parameters = (ParametersForThread) obj;
+            var start = parameters.start;
+            var end = parameters.end;
+            var whichArray = parameters.whichArray;
             
-            foreach (var item in array)
+            for(int i = start; i < end; i++)
             {
                 try
                 {
                     cnt++;
-                    avgTemp += item;
+                    avgTemp += whichArray == SmallBig.Small ? _smallArray[i] : _bigArray[i];
                 }
                 catch (OverflowException e)
                 {
-                    avg += avgTemp/cnt;
+                    if (whichArray == SmallBig.Small) 
+                        AveragesForSmall.Add(avgTemp / cnt);
+                    else 
+                        AveragesForBig.Add(avgTemp/cnt);
                     cnt = 0;
                     avgTemp = 0;
                     Console.WriteLine(e);
                 }
             }
-            return avg;
         }
 
-        public double GetAverageInSmall()
+        public void CalculateAverageWithThreads(SmallBig whichArray)
         {
-            return GetAverage(_smallArray);
+            for (int i = 0; i < 5; i++)
+            {
+                
+            }
+
+            var firstThread = new Thread(new ParameterizedThreadStart(CalculateAverage));
+            var secondThread = new Thread(new ParameterizedThreadStart(CalculateAverage));
+            var thirdThread = new Thread(new ParameterizedThreadStart(CalculateAverage));
+            var fourthThread = new Thread(new ParameterizedThreadStart(CalculateAverage));
+            var fifthThread = new Thread(new ParameterizedThreadStart(CalculateAverage));
+
+            var firstParameters = new ParametersForThread(0, 2000000, whichArray);
+            var secondParameters = new ParametersForThread(2000000, 4000000, whichArray);
+            var thirdParameters = new ParametersForThread(4000000, 6000000, whichArray);
+            var fourthParameters = new ParametersForThread(6000000, 8000000, whichArray);
+            var fifthParameters = new ParametersForThread(8000000, 10000000, whichArray);
+            
+            firstThread.Start(firstParameters);
+        }
+
+        public double CalculateAverageInSmall()
+        {
+            CalculateAverage(new ParametersForThread(0, _smallArray.Length, SmallBig.Small));
+            return AveragesForSmall.Sum(avg => avg / AveragesForSmall.Count);
         }
         
-        public double GetAverageInBig()
+        public double CalculateAverageInBig()
         {
-            return GetAverage(_bigArray);
+            CalculateAverage(new ParametersForThread(0, _bigArray.Length, SmallBig.Big));
+            return AveragesForBig.Sum(avg => avg / AveragesForBig.Count);
         }
     }
 }
