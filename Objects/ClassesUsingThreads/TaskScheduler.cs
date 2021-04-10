31c80f8c5ee65289185f7ffef57e931d80b93524
Георгий Бербат <invariantcalibration@gmail.com>
2021-04-10 10:47:17 +0300

    Удаление ActionWrapper

--- Objects/ClassesUsingThreads/TaskScheduler.cs
+++ Objects/ClassesUsingThreads/TaskScheduler.cs
@@ -1,21 +1,25 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace TwoLayerSolution.ClassesUsingThreads
 {
     public class TaskScheduler : IJobExecutor
     {
-        private ConcurrentQueue<Action> _queue;
-        private ConcurrentDictionary<WaitHandle, Action> _runningTasks;
+        private Queue<Action> _queue;
+        private object _queueLocker;
+        
+        private List<Action> _runningTasks;
+        private object _runningTasksLocker;
 
         private int _maxConcurrent;
         private bool _isMooving;
 
         public TaskScheduler()
         {
-            _queue = new ConcurrentQueue<Action>();
-            _runningTasks = new ConcurrentDictionary<WaitHandle, Action>();
+            _queue = new Queue<Action>();
+            _runningTasks = new List<Action>();
         }
 
         public int Amount { get; private set; }
@@ -25,17 +29,6 @@ namespace TwoLayerSolution.ClassesUsingThreads
         public void Start(int maxConcurrent)
         {
             _maxConcurrent = maxConcurrent;
-            _isMooving = true;
-            var vacantPlaces = _maxConcurrent - _runningTasks.Count;
-            if (Amount > vacantPlaces)
-            {
-                for (int i = 0; i < vacantPlaces; i++)
-                {
-                    
-                }
-            }
-
-
         }
 
         public void Stop()
@@ -47,14 +40,27 @@ namespace TwoLayerSolution.ClassesUsingThreads
         {
             //TODO: Exceptions
             if (action == null) throw new ArgumentNullException(nameof(action));
-            _queue.Enqueue(action);
-            Amount++;
+            ThreadPool.QueueUserWorkItem(state=>{action();});
         }
 
         public void Clear()
         {
-            if (_queue.IsEmpty) return;
-            _queue = new ConcurrentQueue<Action>(); 
+            lock (_queueLocker)
+            {
+                _queue.Clear();
+            }
+        }
+
+        private void OnTaskEnded(object sender, EventArgs e)
+        {
+            if (sender is Action actionWrapperToRemove)
+            {
+                lock (_runningTasksLocker)
+                {
+                    _runningTasks.Remove(actionWrapperToRemove);
+                }
+            }
+            else throw new ArgumentException("Это событие должно быть для Action");
         }
     }
 }
